329c43bb7616b78a06f117c09976508f0136ae85
Giovanni Degiorgi <giovanni.degiorgi@allianz.ch>
2016-02-24 09:47:35 +0100

    initial

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: `<h1>My Second Angular 2 App!</h1>
+               <h4>under title</h4>`
+})
+export class AppComponent { }
